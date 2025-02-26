5f9e4938669cdc4aa556e282eed48dd06a6cada8
Kat0-8 <prokopovich-dan-bsuir@inbox.ru>
2025-02-26 15:31:58 +0300

    Trying to configure sonar

--- src/main/java/com/example/rememberconstellations/repositories/ConstellationsRepository.java
+++ src/main/java/com/example/rememberconstellations/repositories/ConstellationsRepository.java
@@ -11,29 +11,32 @@ public class ConstellationsRepository {
 
     private final List<Constellation> constellations = new ArrayList<>();
 
+    private static final String SUPERGIANT = "Supergiant";
+    private static final String GIANT = "Giant";
+
     public ConstellationsRepository() {
         constellations.add(new Constellation("Orion", "Ori", List.of(
-                new Star("Rigel", "Supergiant", 18, 79,
+                new Star("Rigel", SUPERGIANT, 18, 79,
                         12130, 120000, "Beta"),
-                new Star("Betelgeuse", "Supergiant", 16.5, 764,
+                new Star("Betelgeuse", SUPERGIANT, 16.5, 764,
                         3600, 126000, "Alpha"),
-                new Star("Bellatrix", "Giant", 8.4, 6,
+                new Star("Bellatrix", GIANT, 8.4, 6,
                         22000, 6400, "Gamma"),
-                new Star("Alnilam", "Supergiant", 40, 32.4,
+                new Star("Alnilam", SUPERGIANT, 40, 32.4,
                         27500, 537000, "Epsilon"),
-                new Star("Alnitak", "Supergiant", 18, 20,
+                new Star("Alnitak", SUPERGIANT, 18, 20,
                         33500, 35000, "Zeta"),
-                new Star("Saiph", "Supergiant", 15.5, 22.2,
+                new Star("Saiph", SUPERGIANT, 15.5, 22.2,
                         26500, 56881, "Kappa"),
-                new Star("Mintaka", "Giant", 17.8, 13.1,
+                new Star("Mintaka", GIANT, 17.8, 13.1,
                         18400, 190000, "Delta"),
                 new Star("Hatsya", "Variable", 23.1, 8.3,
                         32500, 68000, "Eta")
         )));
         constellations.add(new Constellation("Ursa Major", "UMa", List.of(
-                new Star("Alkaid", "Giant", 6.1, 3.4,
+                new Star("Alkaid", GIANT, 6.1, 3.4,
                         15540, 594, "Eta"),
-                new Star("Dubhe", "Giant", 5, 30,
+                new Star("Dubhe", GIANT, 5, 30,
                         4685, 360.835, "Alpha")
         )));
     }
