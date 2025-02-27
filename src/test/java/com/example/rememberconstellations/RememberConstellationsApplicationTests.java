2b50c6ba7996b32b32a5d000c0d3e13de1bb69e0
Kat0-8 <prokopovich-dan-bsuir@inbox.ru>
2025-02-27 14:16:07 +0300

    Add tests

---
+++ src/test/java/com/example/rememberconstellations/RememberConstellationsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.rememberconstellations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememberConstellationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //f
+    }
+
+}
