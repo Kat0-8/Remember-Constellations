ffb3a9cb6d23a3a73df7912d80ce7be3e854c537
Kat0-8 <prokopovich-dan-bsuir@inbox.ru>
2025-03-28 12:49:23 +0300

    Logging aspect

---
+++ src/main/java/com/example/rememberconstellations/aspect/LoggingAspect.java
@@ -0,0 +1,46 @@
+package com.example.rememberconstellations.aspect;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Around("within(com.example.rememberconstellations.services..*) || "
+            + "within(com.example.rememberconstellations.controllers..*)")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (logger.isInfoEnabled()) {
+            logger.info("Entering: {} with arguments {}",
+                    joinPoint.getSignature().toShortString(),
+                    Arrays.toString(joinPoint.getArgs()));
+        }
+        Object result = joinPoint.proceed();
+        if (logger.isInfoEnabled()) {
+            logger.info("Exiting: {} with result = {}",
+                    joinPoint.getSignature().toShortString(),
+                    result);
+        }
+        return result;
+    }
+
+    @AfterThrowing(pointcut = "within(com.example.rememberconstellations.services..*) || "
+            + "within(com.example.rememberconstellations.controllers..*)", throwing = "exception")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
+        logger.error("Exception in {} with arguments {}: {}",
+                joinPoint.getSignature().toShortString(),
+                Arrays.toString(joinPoint.getArgs()),
+                exception.getMessage(),
+                exception);
+    }
+}
