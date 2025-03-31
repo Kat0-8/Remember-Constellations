8a6d804de1bfc755f894695087dfd8e3c79bfd95
Kat0-8 <prokopovich-dan-bsuir@inbox.ru>
2025-03-31 15:36:35 +0300

    Logs controller + service

--- src/main/java/com/example/rememberconstellations/controllers/LogsController.java
+++ src/main/java/com/example/rememberconstellations/controllers/LogsController.java
@@ -1,4 +1,37 @@
 package com.example.rememberconstellations.controllers;
 
+import com.example.rememberconstellations.services.LogsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Log API", description = "Get logs")
+@RestController
+@RequestMapping("logs")
 public class LogsController {
+    private final LogsService logsService;
+
+    @Autowired
+    public LogsController(LogsService logsService) {
+        this.logsService = logsService;
+    }
+
+    @Operation(summary = "Get log file for date")
+    @GetMapping("/download")
+    public ResponseEntity<Resource> downloadLogFileForDate(@RequestParam String date) {
+        Resource resource = logsService.getLogFileForDate(date);
+        String fileName = logsService.getFileName(date);
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
 }
--- src/main/java/com/example/rememberconstellations/services/LogsService.java
+++ src/main/java/com/example/rememberconstellations/services/LogsService.java
@@ -1,4 +1,66 @@
 package com.example.rememberconstellations.services;
 
+import com.example.rememberconstellations.exception.InvalidInputException;
+import com.example.rememberconstellations.exception.ResourceNotFoundException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LogsService {
+    private static final String LOGS_DIR = "logs";
+    private static final String LOGS_FILE_NAME = "app.log";
+    private static final DateTimeFormatter INPUT_DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter LOG_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    /* READ */
+
+    public Resource getLogFileForDate(String date) {
+        LocalDate parsedDate = parseDate(date);
+        String formattedDate = parsedDate.format(LOG_DATE_FORMATTER);
+        Path logFilePath = Paths.get(LOGS_DIR, LOGS_FILE_NAME);
+        if (!Files.exists(logFilePath)) {
+            throw new ResourceNotFoundException("Log file " + logFilePath + " was not found");
+        }
+        List<String> requiredLogs = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(logFilePath, StandardCharsets.UTF_8)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith(formattedDate)) {
+                    requiredLogs.add(line);
+                }
+            }
+        } catch (IOException exception) {
+            throw new InvalidInputException("Error reading log file");
+        }
+        if (requiredLogs.isEmpty()) {
+            throw new ResourceNotFoundException("No logs were found for date: " + date);
+        }
+        String contentOfFile = String.join(System.lineSeparator(), requiredLogs);
+        return new ByteArrayResource(contentOfFile.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String getFileName(String date) {
+        LocalDate parsedDate = parseDate(date);
+        return String.format("app-%s.log", parsedDate.format(LOG_DATE_FORMATTER));
+    }
+
+    private LocalDate parseDate(String date) {
+        try {
+            return LocalDate.parse(date, INPUT_DATE_FORMATTER);
+        } catch (DateTimeParseException exception) {
+            throw new InvalidInputException("Invalid date format. Valid format is dd.MM.yyyy");
+        }
+    }
 }
