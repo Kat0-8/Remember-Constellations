cd03c1552b8c87b0f8fd4fbd8ff6983d85404b30
Kat0-8 <prokopovich-dan-bsuir@inbox.ru>
2025-03-31 18:25:18 +0300

    Bulk creating of stars // checkstyle fixes

--- src/main/java/com/example/rememberconstellations/services/StarsService.java
+++ src/main/java/com/example/rememberconstellations/services/StarsService.java
@@ -9,8 +9,6 @@ import com.example.rememberconstellations.models.Star;
 import com.example.rememberconstellations.repositories.StarsRepository;
 import com.example.rememberconstellations.utilities.StarSpecification;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
